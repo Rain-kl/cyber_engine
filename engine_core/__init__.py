7611a276d1ac71a5e77c142c487a506df36bc0a2
ryan <wyrms01@duck.com>
2024-11-13 15:33:07 +0800

    return completion

--- engine_core/__init__.py
+++ engine_core/__init__.py
@@ -1,8 +1,10 @@
+from openai.types.chat import ChatCompletion
+
 from model import InputModel
 from .core import EngineCore
 
 
-async def ponder(input_: InputModel) -> str:
+async def ponder(input_: InputModel) -> ChatCompletion:
     engine = EngineCore(input_)
     chat_completion = await engine.pond()
-    return chat_completion.choices[0].message.content
+    return chat_completion
