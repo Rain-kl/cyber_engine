069ff98eb1219bedced93aebe3e8988fc00d15c8
ryan <wyrms01@duck.com>
2024-11-18 14:28:18 +0800

    用户审计规划

---
+++ useraudit/ext.py
@@ -0,0 +1,19 @@
+
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, Text
+from sqlalchemy.orm import sessionmaker, declarative_base, Query
+
+
+class __SqliteORM:
+    def __init__(self, db_path="./test.db"):
+        self.engine = create_engine(f'sqlite:///{db_path}')
+        self.session = sessionmaker(bind=self.engine)()
+
+    def _create_table(self, base):
+        base.metadata.create_all(self.engine)
+
+    def insert(self, base):
+        self.session.add(base)
+        self.session.commit()
+
+    def query(self, base) -> Query:
+        return self.session.query(base)
---
+++ useraudit/userdb.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, Text
+from sqlalchemy.orm import sessionmaker, declarative_base, Query
+
+try:
+    from .ext import __SqliteORM
+except ImportError:
+    from ext import __SqliteORM
+
+# 创建基类
+Base = declarative_base()
+
+
+# 定义一个User类映射到数据库中的一个表
+class User(Base):
+    """
+    用户表, 用于存储用户ID和标签
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    portrait = Column(String(500))
+
+    def __repr__(self):
+        return f"<User(id='{self.id}', portrait='{self.portrait}')>"
+
+
+class BindID(Base):
+    __tablename__ = 'bind_id'
+
+    bindID = Column(Integer, primary_key=True)
+    id = Column(Integer)
+    platform = Column(String(50))
+
+    def __repr__(self):
+        return f"<BindID(bindID='{self.bindID}', id='{self.id}', platform='{self.platform}')>"
+
+
+class UserDB(__SqliteORM):
+    def __init__(self, db_path="./data/user.db"):
+        super().__init__(db_path)
+        self._create_table(Base)
+
+
+if __name__ == '__main__':
+    db = UserDB("./test.db")
+    # a = db.query(User).filter(User.id == 1).first()
+    # print(a)
